3bd8f88704c3deb19f08c72b3cc4eb182aad6b07
altros.chiho <chihoiuwei@gmail.com>
2026-07-09 21:41:52 +0700

    task sesi 15

--- ecommerce/resources/js/app.js
+++ ecommerce/resources/js/app.js
@@ -1,1 +1,10 @@
-//
+import * as bootstrap from 'bootstrap';
+import Alpine from 'alpinejs';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './ecommerce.js';
+
+window.bootstrap = bootstrap;
+window.Alpine = Alpine;
+
+Alpine.start();
---
+++ ecommerce/resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>@yield('title')</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+
+    @include('partials.navbar')
+
+    @yield('content')
+
+    @include('partials.footer')
+
+</body>
+</html>
